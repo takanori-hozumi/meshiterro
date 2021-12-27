37c58aaf31025b33cce672a22c7dc48e1957a6a0
takanori-hozumi <78337306+takanori-hozumi@users.noreply.github.com>
2021-12-27 13:33:32 +0900

    基礎の完成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '1ecc06c402b160a99f150a5ba0eb748fd7196269b9975d5d9bc5f5019845cc91146900c646fd7ffa3f636fa0d456211b5c05f7ba82da98bbfc2c5277ea46cd2c'
